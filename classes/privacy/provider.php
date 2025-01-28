be58a4585593b05e47f765a8c8f575f7355864a2
mawann <mawan911@yahoo.com>
2025-01-29 02:49:49 +0700

    Update.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace local_mmqpc\privacy;
+namespace local_mawanquizpasswordchanger\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
