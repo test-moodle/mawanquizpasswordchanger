869e80f044a315cecf80471337460930e05e941f
mawann <mawan911@yahoo.com>
2025-01-31 09:34:07 +0700

    Update.

--- version.php
+++ version.php
@@ -24,6 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025013101;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041900;        // Requires this Moodle version.
 $plugin->component = 'local_mawanquizpasswordchanger';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
+$plugin->requires  = 2022041900;        // Requires this Moodle version.
+$plugin->version   = 2025013102;        // The current plugin version (Date: YYYYMMDDXX).
